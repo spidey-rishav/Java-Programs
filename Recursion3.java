1cfa4f5c264ef156b1673e8c39dd526d1a2fd2ae
spidey-rishav <r4rishavmodi@gmail.com>
2025-06-13 20:41:19 +0530

    "changes"

--- Recursion3.java
+++ Recursion3.java
@@ -1,6 +1,4 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class Recursion3 {
@@ -11,28 +9,32 @@ public class Recursion3 {
 
         return x * xPown(x, n-1);
     }
+
     public static int xPown2(int x, int n){
         if (n==0){
             return 1;
         }
-        int pow = xPown(x, n/2);
+        int pow = xPown2(x, n/2);
         if((n&1)==1){
-            return pow * pow * n;
+            return pow * pow * x;
         }
 
         return pow*pow;
     }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter((new OutputStreamWriter(System.out)));
 
         String[] input = br.readLine().split(" ");
 
         int x = Integer.parseInt(input[0]);
         int n = Integer.parseInt(input[1]);
 
-        System.out.println(xPown(x, n));
-        System.out.println(xPown2(x, n));
-
+        bw.write(String.valueOf(xPown(x, n)));
+        bw.newLine();
+        bw.write(String.valueOf(xPown2(x, n)));
+        bw.newLine();
+        bw.flush();
 
     }
 }
---
+++ ReverseSentence.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class ReverseSentence {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String word = br.readLine();
+        String[] input = word.split("\\s+");
+        Stack<String> str = new Stack<>();
+
+        for(int i=0; i<input.length; i++){
+            str.push(input[i]);
+        }
+        while(!str.isEmpty()){
+            System.out.print(str.pop() + " ");
+        }
+        System.out.println();
+    }
+}
