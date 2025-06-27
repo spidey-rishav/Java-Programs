c4b159c4ab8c0d8881d883801b8e21b5d67a3216
spidey-rishav <r4rishavmodi@gmail.com>
2025-06-27 23:15:02 +0530

    edited code

--- ContainMostWater.java
+++ ContainMostWater.java
@@ -1,6 +1,21 @@
 import java.util.*;
 public class ContainMostWater {
+
+    public static int containMostWater(ArrayList<Integer> poles){
+        //bruteforce => O(n^2)
+        int ans = Integer.MIN_VALUE;
+        for(int i=0; i<poles.size(); i++){
+            for(int j=i+1; j<poles.size(); j++){
+                int height = Math.min(poles.get(i), poles.get(j));
+                int width = j-i;
+                ans = Math.max(ans, height*width);
+            }
+        }
+        return ans;
+    }
+
     public static int containMaxWater(int[] ar){
+        //Two Pointers => O(n)
         int p1 = 0, p2 = ar.length-1;
         int ans = Integer.MIN_VALUE;
         while(p1<p2){
@@ -16,9 +31,16 @@ public class ContainMostWater {
         }
         return ans;
     }
+
     public static void main(String[] args){
         int[] ar = {1, 8, 6, 2, 5, 4, 8, 3, 7};
 
+        ArrayList<Integer> poles = new ArrayList<>();
+
+        for(int x : ar){
+            poles.add(x);
+        }
         System.out.println(containMaxWater(ar));
+        System.out.println(containMostWater(poles));
     }
 }
