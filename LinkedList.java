6a9f4b496300b99b9610e6ac28be78249409b3a6
spidey-rishav <r4rishavmodi@gmail.com>
2025-07-03 15:38:50 +0530

    Linked List part 1

---
+++ LinkedList.java
@@ -0,0 +1,191 @@
+import java.util.*;
+
+public class LinkedList {
+    static class Node{
+        int data;
+        Node next;
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+    public static int size;
+
+
+    public void addFirst(int data){
+        Node newNode = new Node(data);
+        size++;
+        if(head == null){
+            head = tail = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public void addLast(int data){
+        Node newNode = new Node(data);
+        size++;
+        if(head == null){
+            head = tail = newNode;
+            return;
+        }
+        tail.next = newNode;
+        tail = newNode;
+    }
+
+    public void add(int idx, int data){
+        if(idx == 0){
+            addFirst(data);
+            return;
+        }
+        Node newNode = new Node(data);
+        size++;
+        Node temp = head;
+        int i=0;
+        while (i<idx-1){
+            temp = temp.next;
+            i++;
+        }
+        newNode.next = temp.next;
+        temp.next = newNode;
+    }
+
+    public int removeFirst(){
+        if(size == 0){
+            System.out.println("LL is empty");
+            return Integer.MIN_VALUE;
+        } else if (size == 1) {
+            int val = head.data;
+            head = tail = null;
+            size--;
+            return val;
+        }
+
+        int val = head.data;
+        head = head.next;
+        size--;
+        return val;
+    }
+
+    public int removeLast(){
+        if(size == 0) {
+            System.out.println("LL is empty!");
+            return Integer.MIN_VALUE;
+        }else if (size == 1){
+            int val = head.data;
+            head = tail = null;
+            size--;
+            return val;
+        }
+
+        Node temp = head;
+        while(temp.next.next != null){
+            temp = temp.next;
+        }
+        int val = temp.next.data; //tail.data
+        temp.next = null;
+        size--;
+        return val;
+    }
+
+    public int search(int key){
+        if(head == null){
+            return -1;
+        }
+        Node temp = head;
+        int i=0;
+
+        while(temp != null){
+            if(temp.data == key){
+                return i;
+            }
+            temp = temp.next;
+            i++;
+        }
+
+        return -1;
+    }
+
+    public static int searchWithRecursion(int key, Node head, int idx){
+        if(head == null){
+            return -1;
+        }
+        if(head.data == key){
+            return idx;
+        }
+        return searchWithRecursion(key, head.next, idx+1);
+    }
+
+    public static void reverseLL(){
+        if(head == null){
+            return;
+        }
+        Node prev = null;
+        Node curr = tail = head;
+        Node next;
+
+        while (curr != null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        head = prev;
+    }
+
+    public static void removeFromEnd(int n){
+        if(n == size){
+            head=head.next;
+            return;
+        }
+        int idx = size-n;
+
+        Node temp = head;
+        for(int i=1; i<idx; i++){
+            temp = temp.next;
+        }
+
+        temp.next = temp.next.next;
+    }
+
+    public static void printll(Node head){
+        if(head == null){
+            System.out.println("LinkedList is empty");
+            return;
+        }
+        Node dummy = head;
+        while (dummy != null){
+            System.out.print(dummy.data + " -> ");
+            dummy = dummy.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args){
+        LinkedList ll = new LinkedList();
+        ll.addFirst(1);
+        ll.addFirst(2);
+        ll.addLast(3);
+        ll.addLast(4);
+        ll.add(2, 5);
+        printll(head);
+//        System.out.println(ll.removeFirst());
+//        System.out.println(ll.removeLast());
+//
+//        printll(head);
+//        System.out.println(size);
+//
+//        System.out.println(ll.search(5));
+//        System.out.println(searchWithRecursion(5, head, 0));
+
+//        reverseLL();
+//        printll(head);
+
+        removeFromEnd(3);
+        printll(head);
+    }
+}
--- Practice.java
+++ Practice.java
@@ -151,13 +151,20 @@ import java.io.*;
 
 class Main
 {
+    public static int fib(int n){
+        if(n==0 || n==1){
+            return n;
+        }
+        return fib(n-1) + fib(n-2);
+    }
     public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
-        while(n!=42){
-            System.out.println(n);
-            n = Integer.parseInt(br.readLine());
-        }
+//        while(n!=42){
+//            System.out.println(n);
+//            n = Integer.parseInt(br.readLine());
+//        }
+        System.out.println(fib(n));
     }
 }
