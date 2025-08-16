ff01021c568581bb32a2ed1dfd1cf8e8ed535f37
spidey-rishav <r4rishavmodi@gmail.com>
2025-08-17 00:54:57 +0530

    remove Cycle in linked list

--- LinkedList2.java
+++ LinkedList2.java
@@ -25,14 +25,44 @@ public class LinkedList2 {
         }
         return false;
     }
-    
+
+    public static void removeCycle(){
+        Node slow = head;
+        Node fast = head;
+
+        boolean isCycle = false;
+        while (fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast){
+                isCycle = true;
+                break;
+            }
+        }
+        if(!isCycle){
+            return;
+        }
+        slow = head;
+        Node prev = null;
+        while (slow != fast){
+            slow = slow.next;
+            prev = fast;
+            fast = fast.next;
+        }
+
+        if(prev != null){
+            prev.next = null;
+        }
+    }
 
     public static void main(String[] args){
         head = new Node(1);
         head.next = new Node(2);
         head.next.next = new Node(3);
-        head.next.next.next = head;
+        head.next.next.next = head.next;
 
+        System.out.println(isCycle());
+        removeCycle();
         System.out.println(isCycle());
     }
 }
